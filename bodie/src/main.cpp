0b72b7b21145f3dde1d06d4b5cd080597419a0d8
GigaVoltFlash <anshukc@gmail.com>
2025-05-26 14:54:20 -0700

    Libraries added

--- bodie/src/main.cpp
+++ bodie/src/main.cpp
@@ -1,34 +1,34 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// // put function declarations here:
+// int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-  // Initialize LED pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-}
+// void setup() {
+//   // put your setup code here, to run once:
+//   int result = myFunction(2, 3);
+//   // Initialize LED pin as an output.
+//   pinMode(LED_BUILTIN, OUTPUT);
+// }
  
-void loop()
-{
-  // Set the LED HIGH
-  digitalWrite(LED_BUILTIN, HIGH);
+// void loop()
+// {
+//   // Set the LED HIGH
+//   digitalWrite(LED_BUILTIN, HIGH);
  
-  // Wait for a second
-  delay(1000);
+//   // Wait for a second
+//   delay(1000);
  
-  // Set the LED LOW
-  digitalWrite(LED_BUILTIN, LOW);
+//   // Set the LED LOW
+//   digitalWrite(LED_BUILTIN, LOW);
  
-   // Wait for a second
-  delay(1000);
+//    // Wait for a second
+//   delay(1000);
 
-}
+// }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// // put function definitions here:
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
 
 
