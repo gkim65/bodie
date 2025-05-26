e26d74d905c6177cbce09e092e1d884b4ad20450
Grace Kim <gracek1459@gmail.com>
2025-05-26 14:25:44 -0700

    blink

--- bodie/src/main.cpp
+++ bodie/src/main.cpp
@@ -6,13 +6,29 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  // Initialize LED pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
+ 
+void loop()
+{
+  // Set the LED HIGH
+  digitalWrite(LED_BUILTIN, HIGH);
+ 
+  // Wait for a second
+  delay(1000);
+ 
+  // Set the LED LOW
+  digitalWrite(LED_BUILTIN, LOW);
+ 
+   // Wait for a second
+  delay(1000);
 
-void loop() {
-  // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
 }
+
+
